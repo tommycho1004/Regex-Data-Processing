90db262cba97cd8b90defc6677c17a8866a24d47
Tommy Cho <tommycho1004@gmail.com>
2022-03-24 23:22:43 -0400

    Finished Covid Section

--- Covid/covid.py
+++ Covid/covid.py
@@ -1,5 +1,6 @@
 import csv
 import re
+import os
 from collections import Counter
 
 def dateSwapper(dateList):
@@ -58,23 +59,27 @@ def symptomsHelper(province):
         symptomSorted = (sorted(Counter(symptom_list).most_common(), key=lambda x: (-x[1], x[0])))
         return symptomSorted[0][0]
 
-with open('covidTrain.csv') as covidfile:
-    reader = csv.reader(covidfile)
-    next(reader)
-    for row in reader:
-        if re.match('[0-9]+-[0-9]+', row[1]):
-            temp = [int(n) for n in row[1].split('-')]
-            row[1] = round(sum(temp)/2)
-            #print(row[1])
-        row[8] = ".".join(dateSwapper(row[8].split('.')))
-        row[9] = ".".join(dateSwapper(row[8].split('.')))
-        row[10] = ".".join(dateSwapper(row[8].split('.')))
-        if re.match('NaN', row[6]):
-            row[6] = latitudeAvg(row[4])
-        if re.match('NaN', row[7]):
-            row[7] = longitudeAvg(row[4])
-        if re.match('NaN', row[3]):
-            row[3] = commonCity(row[4])
-        if re.match('NaN', row[11]):
-            row[11] = symptomsHelper(row[4])
+
+with open('covidResult.csv', 'w', newline='') as outFile:
+    writer = csv.writer(outFile, delimiter=',')
+    with open('covidTrain.csv') as covidfile:
+        reader = csv.reader(covidfile)
+        writer.writerow(next(reader))
+        for row in reader:
+            if re.match('[0-9]+-[0-9]+', row[1]):
+                temp = [int(n) for n in row[1].split('-')]
+                row[1] = round(sum(temp)/2)
+            row[8] = ".".join(dateSwapper(row[8].split('.')))
+            row[9] = ".".join(dateSwapper(row[8].split('.')))
+            row[10] = ".".join(dateSwapper(row[8].split('.')))
+            if re.match('NaN', row[6]):
+                row[6] = latitudeAvg(row[4])
+            if re.match('NaN', row[7]):
+                row[7] = longitudeAvg(row[4])
+            if re.match('NaN', row[3]):
+                row[3] = commonCity(row[4])
+            if re.match('NaN', row[11]):
+                row[11] = symptomsHelper(row[4])
+            writer.writerow(row)
+
             
